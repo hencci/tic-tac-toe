92e31f66f2e83843caf3cae43d9466fb503f547a
hencci <hencci20@gmail.com>
2024-07-26 09:13:10 +0100

    Assigned variabled to some DOM selections

---
+++ script.js
@@ -0,0 +1,17 @@
+const grids = document.querySelectorAll(".grid");
+const winText = document.querySelector("#winText");
+const restartBtn = document.querySelector(".restartButton");
+const startButton = document.querySelector(".startButton")
+const winConditions =  [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
